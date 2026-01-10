b9a08c55ff9687443fe424efac5d9eaa342fa601
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-10 17:36:30 +0900

    feat:train.js done

---
+++ train.js
@@ -0,0 +1,25 @@
+/*TASK G:
+
+Yagona parametrga ega function tuzing.
+Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi. */
+
+function getHighestIndex(myArray)  {
+  let max = myArray[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < myArray.length; i++)
+{
+    if (myArray[i] > max) {
+      max = myArray[i];
+      maxIndex = i;
+    }
+  }
+     return maxIndex;
+}
+
+console.log(getHighestIndex([1, 5, 4, 14, 32]));
