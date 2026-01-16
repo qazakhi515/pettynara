5e295e2f450a6dadc2977edecf5a102599a34596
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-16 16:01:55 +0900

    feat: create routing and member controllers

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /* 1- ENTRANCE */
 const app = express();
-console.log("__dirname", __dirname );
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 /* 2- SESSION */
+// authentication 
+// authorization
 /* 3- VIEWS */
-app.set("view", path.join(__dirname, "views"));
+app.set("view", path.join(__dirname, "views"));   
 app.set("view engine", "ejs");
 /* 4- ROUTERS */
+app.use("/", router);    // Middleware Design Pattern
+
 export default app;   // common js da module.exports kabi qilinar edi esma js da esa export default boladi.
---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from "express";
+const router = express.Router();
+import memberController from "./controllers/memberController";
+
+router.get("/", memberController.goHome);
+
+router.get("/login", memberController.getLogin);
+
+router.get("/signup",memberController.getSignup);
+
+export default router;
