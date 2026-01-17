9c96479ed9613d36100bd9fb72f34243e9f4d5d1
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-18 02:11:56 +0900

    feat: develop sign up process

--- src/controllers/restaurantConroller.ts
+++ src/controllers/restaurantConroller.ts
@@ -1,6 +1,8 @@
 import {Request, Response } from "express";
 import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { MemberInput } from "../libs/types/member";
+import {MemberType} from "../libs/types/enums/member.enum"
 
 const restaurantController: T  = {};
 restaurantController.goHome = (req:Request, res: Response) => {
@@ -22,8 +24,6 @@ restaurantController.getLogin = (req:Request, res: Response) => {
   } 
 }
 
-
-
 restaurantController.getSignup = (req:Request, res: Response) => {
   try {
     res.send("Signup Page");
@@ -41,12 +41,22 @@ restaurantController.processLogin = (req:Request, res: Response) => {
   } 
 };
 
-restaurantController.processSignup = (req:Request, res: Response) => {
+restaurantController.processSignup = async (req:Request, res: Response) => {
   try {
     console.log("processSignup");
-    res.send("Done!");
+    console.log("body", req.body);
+
+    const newMember:MemberInput = req.body;
+    newMember.memberType = MemberType.RESTAURANT;
+
+    const memberService = new MemberService();
+    const result = await memberService.processSignup(newMember);
+
+    res.send(result);
   } catch(err) {
     console.log("Error, processLogin:", err)
+    res.send(err);
   } 
 };
+
 export default restaurantController;
