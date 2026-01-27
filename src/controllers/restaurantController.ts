0cd2f40d8163bc9c7a1e1b129c9fff78c4a1b68a
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-28 03:04:18 +0900

    feat: create session checkAuthSession api

--- src/controllers/restaurantController.ts
+++ src/controllers/restaurantController.ts
@@ -3,6 +3,7 @@ import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import {MemberType} from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 const restaurantController: T  = {};
@@ -76,5 +77,20 @@ restaurantController.processLogin = async (
   } 
 };
 
+
+restaurantController.checkAuthSession = async (
+  req:AdminRequest, 
+  res: Response
+) => {
+  try {
+    console.log("checkAuthSession");
+    if(req.session?.member) res.send(`Hi, ${req.session.member.memberNick}`);
+    else res.send(`<script> alert ("${Message.NOT_AUTHENTICATED}")</script>`)
+  /// 1.1 dan kelgan natujani front end ga jonatib yuboryopmiz.
+  } catch(err) {
+    console.log("Error, checkAuthSession:", err)
+    res.send(err);
+  } 
+};
 
 export default restaurantController; 
--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -18,6 +18,7 @@ export enum Message {
     NO_MEMBER_NICK = "No member with that member nick",
     USED_NICK_PHONE = "You are inserting already used nick or phone",
     WRONG_PASSWORD = "Wrong password, please try again",
+    NOT_AUTHENTICATED = "You are not authenticated, Please login first"
 }
 
 
