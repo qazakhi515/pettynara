70d91ba22afd76f41ccb9c62b0b73fd3770bdaae
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-30 15:59:04 +0900

    feat: locals variable integration

--- src/controllers/restaurantController.ts
+++ src/controllers/restaurantController.ts
@@ -1,4 +1,4 @@
-import {Request, Response } from "express";
+import {NextFunction, Request, Response } from "express";
 import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
@@ -88,8 +88,7 @@ restaurantController.processLogin = async (
     err instanceof Errors ? err.message: Message.SOMETHING_WENT_WRONG;
     res.send(
       `<script> alert ("${message}"); window.location.replace('admin/login') </script>`
-    );
-    
+    );    
   } 
 };
 
@@ -122,6 +121,22 @@ restaurantController.checkAuthSession = async (
   } catch(err) {
     console.log("Error, checkAuthSession:", err)
     res.send(err);
+  } 
+};
+
+restaurantController.verifyRestaurant = (
+  req:AdminRequest, 
+  res: Response,
+  next: NextFunction
+) => {
+   if(req.session?.member?.memberType === MemberType.RESTAURANT){
+      req.member = req.session.member;
+      next();
+  } else {
+    const message = Message.NOT_AUTHENTICATED;
+    res.send(
+      `<script> alert ("${message}"); window.location.replace('/admin/login') (</script>`
+    );
   } 
 };
 
