032ca84c79ba809ba1b02fd100d809a91f813307
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-30 18:11:37 +0900

    feat:upload files into server via multer

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -22,7 +22,7 @@ productController.getAllProducts = async (req:AdminRequest, res: Response) => {
 productController.createNewProduct = async (req:Request, res: Response) => {
   try {
     console.log("createNewProduct");
-   
+   res.send("done");
   } catch(err) {
     console.log("Error, createNewProduct:", err)
    if(err instanceof Errors) res.status(err.code).json(err)
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurantController";
 import productController from "./controllers/productController";
+import makeUploader from "./libs/utils/uploader";
 
 /* Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -11,7 +12,7 @@ routerAdmin
 
 routerAdmin
     .get("/signup",restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+    .post("/signup",makeUploader("members").single("productImage"), restaurantController.processSignup);
 
     routerAdmin.get("/logout",restaurantController.logout)
     routerAdmin.get("/check-me",restaurantController.checkAuthSession);
@@ -21,9 +22,19 @@ routerAdmin.get(
     "/product/all",
      restaurantController.verifyRestaurant,
      productController.getAllProducts
-    );
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+);
+routerAdmin.post(
+    "/product/create", 
+    restaurantController.verifyRestaurant,
+    // uploadProductImage.single('productImage'),
+    makeUploader("products").array("productImages",1),
+    productController.createNewProduct
+);
+routerAdmin.post(
+    "/product/:id",
+    restaurantController.verifyRestaurant,
+    productController.updateChosenProduct
+);
 
     /* User */
 
