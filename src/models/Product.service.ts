0f621a55b7367c56db7987e5ea48383b6cd93334
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-30 19:17:00 +0900

    feat: Product schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,2 +1,11 @@
-class ProductServise{};
+import ProductModel from "../schema/Product.model";
+
+class ProductServise{
+    private readonly productModel;
+
+    constructor() {
+        this.productModel =ProductModel;
+
+    }
+};
 export default ProductServise;
