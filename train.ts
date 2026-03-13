d940dadde729f97bfd01224337b730bad677572c
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-03-13 18:16:35 +0900

    fix: Task ZD has done

--- train.ts
+++ train.ts
@@ -1,8 +1,33 @@
-function randomBetween(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// TASK ZD
+
+// Shunday function yozing. Bu function o'ziga, parametr sifatida
+// birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
+// qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
+// shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
+// yangilangan arrayni qaytarsin.
+
+// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
+
+// Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
+// Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
+// yangilangan arrayni qaytarmoqda.
+
+function changeNumberInArray(
+  index: number,
+  arr: number[],
+  newNumber: number,
+): number[] {
+  arr[index] = newNumber;
+  return arr;
 }
 
-console.log(randomBetween(40, 60));
+console.log(changeNumberInArray(1, [1, 3, 7, 2], 2));
+
+// function randomBetween(min: number, max: number): number {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// console.log(randomBetween(40, 60));
 
 // function findIntersection(arr1: number[], arr2: number[]): number[] {
 //   const oxshash = new Set(arr2);
